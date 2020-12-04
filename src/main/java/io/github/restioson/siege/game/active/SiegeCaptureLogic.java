941ac5b697958534b6fbc78c81ee7e91fc861bbf
Restioson <restiosondev@gmail.com>
2020-12-04 22:30:31 +0200

    tweak capture time (60s -> 40s)

--- src/main/java/io/github/restioson/siege/game/active/SiegeCaptureLogic.java
+++ src/main/java/io/github/restioson/siege/game/active/SiegeCaptureLogic.java
@@ -30,7 +30,7 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class SiegeCaptureLogic {
-    public static final int CAPTURE_TIME_TICKS = 20 * 60;
+    public static final int CAPTURE_TIME_TICKS = 20 * 40;
 
     private final GameSpace gameSpace;
     private final SiegeActive game;
